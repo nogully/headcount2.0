556f328a699bea2d4ff998ae53fced58d2d8d58c
Spencer Herms <slherms@gmail.com>
2018-01-22 19:33:26 -0700

    first 5 tests passing

--- src/helper.js
+++ src/helper.js
@@ -1,21 +1,36 @@
 export default class DistrictRepository {
   constructor(data) {
-    this.data = this.filter(data)
-
+    this.data = this.filter(data);
   }
   
   filter(data) {
     return data.reduce((acc, obj) => {
       if (!acc[obj.Location]) {
-        acc[obj.Location] = {}
+        let newKey = obj.Location.toUpperCase();
+
+        acc[newKey] = {
+          location: newKey,
+          data: {}
+        };
       }
       return acc;
-    }, {})
+    }, {});
   }
 
-  findByName(string){
+  findByName(string) {
 
+    if (string) {
+      const keys =  Object.keys(this.data);
+      const foundString = keys.includes( string.toUpperCase() );
+
+      if (foundString) {
+        return keys.reduce( (accu, location) => {
+          if ( location === string.toUpperCase() ) {
+            accu = this.data[location];
+          }
+          return accu;
+        }, {} );
+      }
+    }
   }
-
-
 }
