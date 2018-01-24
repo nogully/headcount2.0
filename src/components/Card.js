8778aab9e3bb8d793ad5ac20feb1ebbe0858d70a
Nora Gully <nora.gully@gmail.com>
2018-01-23 18:27:02 -0700

    Generate cards that have key value pairs of year and percent from the data object | => | => they're showing up in the card components which are rendering onscreen

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ data }) => {
+  const yearsArray = Object.keys(data.data);
+  const percentArray = Object.values(data.data);
+  const elements = percentArray.map( (percent, index) => {
+    return <div key={index}>{yearsArray[index]} : {percent} </div>
+  })
+
   return (
     <article>
-      <h3>{props.data.location}</h3>
-      <p></p>
+      <h3>{ data.location }</h3>
+      {elements}
     </article>
   )
 }
