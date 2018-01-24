175b55b84ea5893435cfbbc614a4353a3b681da3
Nora Gully <nora.gully@gmail.com>
2018-01-23 17:53:24 -0700

    Create CardContainer.js and Card.js components and pass down mockData from App to Card file. Move App.css into styles directory and create components directory

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <article>
+      <h3>{props.data.location}</h3>
+      <p></p>
+    </article>
+  )
+}
+export default Card;
