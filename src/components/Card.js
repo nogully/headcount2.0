06b7c818b939cc1b8fb51ca8aaf96c2990bb9a1f
Spencer Herms <slherms@gmail.com>
2018-01-23 23:42:25 -0700

    sets up enzmye, sets up tests for Card and CardContainer, adds mock data for testing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import '../styles/Card.css';
+import React from "react";
+import "../styles/Card.css";
 
 const Card = ({ data }) => {
   const yearsArray = Object.keys(data.data);
   const percentArray = Object.values(data.data);
-  const elements = percentArray.map( (percent, index) => {
-    return <div className="card-data-row" key={index}><span className="years">{yearsArray[index]} : </span><span className="percents">{percent}</span> </div>
-  })
+  const elements = percentArray.map((percent, index) => {
+    return (
+      <div className="card-data-row" key={index}>
+        <span className="years">{yearsArray[index]} : </span>
+        <span className="percents">{percent}</span>{" "}
+      </div>
+    );
+  });
 
   return (
     <article>
-      <h3>{ data.location }</h3>
-      <div className="element-wrapper">
-        { elements }
-      </div>
+      <h3>{data.location}</h3>
+      <div className="element-wrapper">{elements}</div>
     </article>
-  )
-}
+  );
+};
 export default Card;
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import Card from './Card';
-import '../styles/CardContainer.css';
+import React from "react";
+import Card from "./Card";
+import "../styles/CardContainer.css";
 
 const CardContainer = ({ data }) => {
-  const renderedCards = data.map((district, index) => <Card data={district} key={index} />
-  )
+  const renderedCards = data.map((district, index) => (
+    <Card data={district} key={index} />
+  ));
 
-  return (
-    <div className="CardContainer">
-    { renderedCards }
-    </div>
-  )
-}
+  return <div className="CardContainer">{renderedCards}</div>;
+};
 
 export default CardContainer;
