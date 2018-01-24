fd3a8ee94405d2870c9829b842d66c9938923917
Spencer Herms <slherms@gmail.com>
2018-01-24 11:28:56 -0700

    validtes all prop types for Card and CardContainer

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../styles/Card.css";
+import PropTypes from "prop-types";
 
 const Card = ({ data }) => {
   const yearsArray = Object.keys(data.data);
   const percentArray = Object.values(data.data);
+
   const elements = percentArray.map((percent, index) => {
     return (
       <div className="card-data-row" key={index}>
@@ -20,4 +22,26 @@ const Card = ({ data }) => {
     </article>
   );
 };
+
+Card.prototype = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      location: PropTypes.string.isRequired,
+      data: PropTypes.shape({
+        2004: PropTypes.number.isRequired,
+        2005: PropTypes.number.isRequired,
+        2006: PropTypes.number.isRequired,
+        2007: PropTypes.number.isRequired,
+        2008: PropTypes.number.isRequired,
+        2009: PropTypes.number.isRequired,
+        2010: PropTypes.number.isRequired,
+        2011: PropTypes.number.isRequired,
+        2012: PropTypes.number.isRequired,
+        2013: PropTypes.number.isRequired,
+        2014: PropTypes.number.isRequired
+      }).isRequired
+    })
+  ).isRequired
+}
+
 export default Card;
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import "../styles/CardContainer.css";
+import PropTypes from "prop-types";
 
 const CardContainer = ({ data }) => {
   const renderedCards = data.map((district, index) => (
@@ -8,6 +9,27 @@ const CardContainer = ({ data }) => {
   ));
 
   return <div className="CardContainer">{renderedCards}</div>;
+};
+
+CardContainer.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      location: PropTypes.string.isRequired,
+      data: PropTypes.shape({
+        2004: PropTypes.number.isRequired,
+        2005: PropTypes.number.isRequired,
+        2006: PropTypes.number.isRequired,
+        2007: PropTypes.number.isRequired,
+        2008: PropTypes.number.isRequired,
+        2009: PropTypes.number.isRequired,
+        2010: PropTypes.number.isRequired,
+        2011: PropTypes.number.isRequired,
+        2012: PropTypes.number.isRequired,
+        2013: PropTypes.number.isRequired,
+        2014: PropTypes.number.isRequired
+      }).isRequired
+    })
+  ).isRequired
 };
 
 export default CardContainer;
