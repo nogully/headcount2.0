c2389f8b6a31b07961f2a19f8c8be4da39de49d6
Nora Gully <nora.gully@gmail.com>
2018-01-24 19:41:47 -0700

    Create comparison function that makes stupid pointless division between district data

--- src/helper.js
+++ src/helper.js
@@ -50,4 +50,29 @@ export default class DistrictRepository {
         }).map(district => this.data[district]);
       }
     }
+
+    findAverage(districtName) {
+      const foundDistrict = this.findByName(districtName);
+      const percents = Object.values(foundDistrict.data)
+      const average =  percents.reduce( (sum, percent) => {
+        sum += percent;
+        return sum;
+      }, 0) / percents.length
+      return Math.round(average * 1000) / 1000
+    }
+
+    compareDistrictAverages(dist1, dist2) {
+      const foundDistrict1 = this.findByName(dist1);
+      const foundDistrict2 = this.findByName(dist2);
+      const comparisonObj = {}
+      
+      if (foundDistrict1 && foundDistrict2){
+        comparisonObj[foundDistrict1.location] = this.findAverage(foundDistrict1.location);
+        comparisonObj[foundDistrict2.location] = this.findAverage(foundDistrict2.location);
+        comparisonObj.compared = Math.round((this.findAverage(dist1) / this.findAverage(dist2)) * 1000) / 1000
+      }
+      return comparisonObj;
+    }
+
+
   }
