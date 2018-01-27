5de4371aecc665b1d898741aeef6a664272158b0
Nora Gully <nora.gully@gmail.com>
2018-01-27 12:22:10 -0700

    Create Comparison.test.js file with tests passing. Add state.display and event simulation tests to Search.test.js which are inexplicably not passing

--- src/test/unit/Comparison.test.js
+++ src/test/unit/Comparison.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import React from "react";
-import ReactDOM from "react-dom";
 import { shallow, mount, render } from "enzyme";
 import Comparison from "../../components/Comparison";
 import { selected, containerData, cardData } from './mockData';
@@ -23,9 +22,16 @@ describe("Comparison", () => {
     expect(wrapper).toBeDefined();
   });
 
-  // it('should make 2 cards if 2 cards are passed into it', () => {
-  //   expect(wrapper.find('Card').length).toEqual(2)
-  // })
+  it('should make 2 Card components if it receives a full selected array', () => {
+    expect(wrapper.find('Card').length).toEqual(2)
+  })
 
-  // should render a comparision card
+  it('should render an average card in the middle of two Card components', () => {
+    expect(wrapper).toMatchSnapshot();
+  }) 
+
+  it('should display an average of the two districts', () => {
+    expect(wrapper.find('.compared').text()).toEqual('1.302')
+  })
+
 });
